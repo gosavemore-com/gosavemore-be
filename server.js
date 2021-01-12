76e8563e2b19154c1b0547acbd8377aadfe73e8f
denmercs <denmercs@gmail.com>
2021-01-12 15:00:24 -0600

    added user model, controller, and routes

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const cors = require("cors");
+const userRoutes = require("./routes/userRoutes.js");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api/users", userRoutes);
+
+module.exports = app;
