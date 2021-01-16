21b79e1a25b6d6df70a5494558954ff9b1d14bd9
denmercs <denmercs@gmail.com>
2021-01-16 11:25:08 -0600

    made changes in product routes and added product controller

---
+++ controllers/productController.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const asyncHandler = require("express-async-handler");
+const Product = require("../models/productModel");
+
+// @desc Fetch all product
+// @route GET /api/products
+// @access public
+const fetchProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({});
+  res.json(products);
+});
+
+// @desc Fetch single product
+// @route GET /api/products/:id
+// @access public
+const fetchProductId = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
+
+module.exports = { fetchProducts, fetchProductId };
