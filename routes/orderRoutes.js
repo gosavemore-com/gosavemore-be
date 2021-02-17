d5323cc9bdf68415901e9173d722ccd70cef6373
denmercs <denmercs@gmail.com>
2021-02-17 02:29:01 -0600

    added updateOrderToPaid controller and update route

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -6,12 +6,13 @@ const {
   addOrderItems,
   getOrders,
   getOrderById,
+  updateOrderToPaid,
 } = require("../controllers/orderController.js");
 
 router.route("/").post(protect, addOrderItems).get(protect, admin, getOrders);
 // router.route("/myorders").get(protect, getMyOrders);
 router.route("/:id").get(protect, getOrderById);
-// router.route("/:id/pay").put(protect, updateOrderToPaid);
+router.route("/:id/pay").put(protect, updateOrderToPaid);
 // router.route("/:id/deliver").put(protect, admin, updateOrderToDelivered);
 
 module.exports = router;
